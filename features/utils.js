7cafbbc5201fcd867ac234c91c268c3b6056e397
Victor Oliva <olivarra1@gmail.com>
2019-11-02 18:57:25 +0100

    Fix setting extreme dates

--- features/utils.js
+++ features/utils.js
@@ -1,25 +1,25 @@
 const {
-  setHours,
-  setMinutes,
   setSeconds,
-  setDate,
-  setMonth,
-  setYear
+  set
 } = require("date-fns");
 
 function applyTimeString(date, text) {
   const time = text.split(":");
-  date = setHours(date, Number(time[0]));
-  date = setMinutes(date, Number(time[1]));
+  date = set(date, {
+    hours: Number(time[0]),
+    minutes: Number(time[1])
+  });
   time[2] && (date = setSeconds(date, Number(time[2])));
 
   return date;
 }
 function applyDateString(date, text) {
   const value = text.split("-");
-  date = setDate(date, Number(value[0]));
-  date = setMonth(date, Number(value[1]) - 1);
-  date = setYear(date, Number(value[2]));
+  date = set(date, {
+    date: Number(value[0]),
+    month: Number(value[1]) - 1,
+    year: Number(value[2])
+  });
 
   return date;
 }
