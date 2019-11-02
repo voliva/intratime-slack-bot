67eead8de0acefe1dc5ced678647b2548f7b1d0b
Víctor Oliva <olivarra1@gmail.com>
2019-11-02 11:21:34 +0100

    React to reminders

--- features/reminders.js
+++ features/reminders.js
@@ -1,9 +1,9 @@
-const { set, isToday, isWeekend } = require("date-fns");
+const { set, isToday, isWeekend, format } = require("date-fns");
 const cron = require("node-cron");
 const request = require('request');
 const util = require('util');
 const post = util.promisify(request.post);
-const { getStatus, Action } = require('../intratime');
+const { getStatus, Action, submitClocking, fillAllDay } = require('../intratime');
 
 const timeRegex = /^([01]\d|2[0-3]):[0-5]\d(:[0-5]\d)?$/;
 
@@ -86,13 +86,21 @@ async function processIM(event, db) {
   const { actions, response_url } = event;
   const action = actions[0];
 
+  const updateMessage = text => post(response_url, {
+    body: {
+      replace_original: "true",
+      text
+    },
+    json: true
+  });
+
   const user = db
     .get('users')
     .find({ id: event.channel.id })
     .value();
 
   if(!user) {
-    return;
+    return updateMessage(`I'm sorry but something wrong has happened - I don't know who you are. Try again later.`);
   }
 
   if (action.action_id === "delete-reminder") {
@@ -108,18 +116,38 @@ async function processIM(event, db) {
       })
       .write();
 
-    await post(response_url, {
-      body: {
-        replace_original: "true",
-        text: `Done - I won't remind you at ${reminder}`
-      },
-      json: true
-    });
+    await updateMessage(`Done - I won't remind you at ${reminder}`);
+  }
+  if (action.action_id === "reminder-action") {
+    const value = action.value.split("/");
+    const date = new Date(`${value[3]}-${value[2]}-${value[1]}`);
+
+    if(!isToday(date)) {
+      return updateMessage(`Sorry, but that action has expired - Use regular commands instead`);
+    }
+
+    const actionMsg = updateMessage(`Sure - Give me just a second`);
+    await submitClocking(user.token, Action[value[0]], new Date(), false);
+
+    await actionMsg;
+    return updateMessage(`Done! I've performed the action ${value[0]} for you`);
+  }
+  if (action.action_id === "reminder-fill") {
+    const value = action.value.split("/");
+    const dateStr = `${value[3]}-${value[2]}-${value[1]}`;
+    const date = new Date(dateStr);
+
+    const actionMsg = updateMessage(`Sure - Give me just a few seconds`);
+    await fillAllDay(user.token, date);
+
+    await actionMsg;
+    return updateMessage(`Done! I've filled all the intratimes of ${isToday(date) ? 'today' : dateStr} for you`);
   }
 }
 
 async function sendReminders(db, slackWeb) {
   const now = Date.now();
+  const today = format(now, 'dd/MM/yyyy')
   if(isWeekend(now)) {
     return;
   }
@@ -179,9 +207,10 @@ async function sendReminders(db, slackWeb) {
                 type: "button",
                 text: {
                   type: 'plain_text',
-                  text: nextAction
+                  text: nextAction + ' (Now)'
                 },
-                value: nextAction
+                value: nextAction + '/' + today,
+                action_id: 'reminder-action'
               }
             ]
           }],
@@ -205,7 +234,8 @@ async function sendReminders(db, slackWeb) {
                 type: 'plain_text',
                 text: 'Fill all day'
               },
-              value: 'FillAllDay'
+              value: 'FillAllDay/' + today,
+              action_id: 'reminder-fill'
             },
             {
               type: "button",
@@ -213,7 +243,8 @@ async function sendReminders(db, slackWeb) {
                 type: 'plain_text',
                 text: 'Check In'
               },
-              value: 'CheckIn'
+              value: 'CheckIn/' + today,
+              action_id: 'reminder-action'
             }
           ]
         }],
@@ -230,8 +261,10 @@ async function sendReminders(db, slackWeb) {
   }
 }
 
-const setupReminders = (db, slackWeb) =>
+const setupReminders = (db, slackWeb) => {
+  sendReminders(db, slackWeb);
   cron.schedule("* * * * *", () => sendReminders(db, slackWeb));
+}
 
 module.exports = {
   setupReminders,
